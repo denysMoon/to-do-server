e11a04d4d4cd1e665b04277c4a2986a58b2fc227
denysMoon <denysulvanskyi@gmail.com>
2021-10-05 19:12:20 +0300

    fix delete option

--- routes/list.js
+++ routes/list.js
@@ -26,9 +26,10 @@ router.post('/', async (req, res)=>{
     }
 })
 
-router.delete('/:value', async (req, res)=>{
+router.delete('/:postId', async (req, res)=>{
+    debugger
     try{
-        const removedList = await List.remove({_id: req.params.value})
+        const removedList = await List.remove({_id: req.params.postId})
         res.json(removedList)
     } catch(err){
         res.json({message: err})
